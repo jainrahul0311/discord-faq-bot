ce6d80813c95155b0c271a743fea277d11cc2a82
Rahul Jain <jainrahul0311@gmail.com>
2021-01-22 21:47:48 +0530

    fix the add command for 1 line word

--- src/main/java/com/discord/faq_bot/event/MessageListener.java
+++ src/main/java/com/discord/faq_bot/event/MessageListener.java
@@ -27,103 +27,103 @@ public abstract class MessageListener {
     public Mono<Void> processCommand(Message eventMessage) {
 
         Optional<User> author = eventMessage.getAuthor();
-        try{
+        try {
 
-        String content = eventMessage.getContent().toLowerCase();
+            String content = eventMessage.getContent().toLowerCase();
 
-        if (author.isPresent()) {
-            User user = author.get();
-            logger.info("Message : "+ content);
+            if (author.isPresent()) {
+                User user = author.get();
+                logger.info("Message : " + content);
 
-            if (!user.isBot()) {
+                if (!user.isBot()) {
 
-                String resp = "";
+                    String resp = "";
 
-                List<CustomCommand> all = commandRepository.findAll();
+                    List<CustomCommand> all = commandRepository.findAll();
 
-                if (content.contains("!list-cmd")) {
-                    String tempResp = "Special Non-Removable Commands are : \n\n!add-cmd\n!rm-cmd\n\n";
-                    resp = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.joining("\n"));
-                    if (resp.length() > 0) {
-                        resp = "List of Custom Commands are : \n\n" + resp;
-                    }
-                    resp = tempResp + resp;
-                } else if (content.contains("!add-cmd")) {
+                    if (content.contains("!list-cmd")) {
+                        String tempResp = "Special Non-Removable Commands are : \n\n!add-cmd\n!rm-cmd\n\n";
+                        resp = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.joining("\n"));
+                        if (resp.length() > 0) {
+                            resp = "List of Custom Commands are : \n\n" + resp;
+                        }
+                        resp = tempResp + resp;
+                    } else if (content.contains("!add-cmd")) {
 
-                    String[] splittedContent = content.split(" ");
-                    if (splittedContent[0].equals("!add-cmd") && splittedContent.length > 3) {
-                        String identifier = splittedContent[1].toLowerCase();
-                        List<CustomCommand> cmdList = commandRepository.getByIdentifierEquals(identifier);
-                        if (cmdList.size() == 0) {
-                            CustomCommand customCommand = new CustomCommand(identifier, content.substring(splittedContent[0].length() + splittedContent[1].length() + 1), user.getId().asLong(), false);
-                            commandRepository.save(customCommand);
-                            resp = "Command added Successfully";
+                        String[] splittedContent = content.split(" ");
+                        if (splittedContent[0].equals("!add-cmd") && splittedContent.length >= 3) {
+                            String identifier = splittedContent[1];
+                            List<CustomCommand> cmdList = commandRepository.getByIdentifierEquals(identifier);
+                            if (cmdList.size() == 0) {
+                                CustomCommand customCommand = new CustomCommand(identifier, content.substring(splittedContent[0].length() + splittedContent[1].length() + 1), user.getId().asLong(), false);
+                                commandRepository.save(customCommand);
+                                resp = "Command added Successfully";
+                            } else {
+                                resp = cmdList.size() + " existing command found with name : " + identifier;
+                            }
                         } else {
-                            resp = cmdList.size() + " existing command found with name : " + identifier;
+                            resp = "For using !add cmd message should be in below format \n !add <trigger_name> <message>";
+                        }
+                    } else if (content.contains("!rm-cmd")) {
+                        String[] splittedContent = content.split(" ");
+                        if (splittedContent[0].equals("!rm-cmd") && splittedContent.length == 2) {
+                            String identifier = splittedContent[1];
+                            List<CustomCommand> cmdList = commandRepository.getByIdentifierEquals(identifier);
+                            if (cmdList.size() > 0) {
+                                CustomCommand customCommand = cmdList.get(0);
+                                commandRepository.delete(customCommand);
+                                resp = identifier + " Command was removed Successfully";
+                            } else {
+                                resp = "No existing Command found with name : " + identifier;
+                            }
+                        } else {
+                            resp = "For using !rm-cmd Command message should be in below format \n !rm-cmd <trigger_name>";
                         }
                     } else {
-                        resp = "For using !add cmd message should be in below format \n !add <trigger_name> <message>";
-                    }
-                } else if (content.contains("!rm-cmd")) {
-                    String[] splittedContent = content.split(" ");
-                    if (splittedContent[0].equals("!rm-cmd") && splittedContent.length == 2) {
-                        String identifier = splittedContent[1].toLowerCase();
-                        List<CustomCommand> cmdList = commandRepository.getByIdentifierEquals(identifier);
-                        if (cmdList.size() > 0) {
-                            CustomCommand customCommand = cmdList.get(0);
-                            commandRepository.delete(customCommand);
-                            resp = identifier + " Command was removed Successfully";
-                        } else {
-                            resp = "No existing Command found with name : " + identifier;
-                        }
-                    } else {
-                        resp = "For using !rm-cmd Command message should be in below format \n !rm-cmd <trigger_name>";
-                    }
-                } else {
-                    int i = content.indexOf(" ");
-                    if (i != -1) {
-                        Set<String> dbIdentifier = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.toSet());
+                        int i = content.indexOf(" ");
+                        if (i != -1) {
+                            Set<String> dbIdentifier = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.toSet());
 
-                        Set<String> collect1 = Arrays.stream(content.split(" ")).collect(Collectors.toSet());
-                        for (String c : collect1) {
-                            if (dbIdentifier.contains(c.toLowerCase())) {
-                                List<CustomCommand> byIdentifierEquals = commandRepository.getByIdentifierEquals(c);
-                                CustomCommand customCommand = byIdentifierEquals.get(0);
-                                resp = customCommand.getMessage();
+                            Set<String> collect1 = Arrays.stream(content.split(" ")).collect(Collectors.toSet());
+                            for (String c : collect1) {
+                                if (dbIdentifier.contains(c)) {
+                                    List<CustomCommand> byIdentifierEquals = commandRepository.getByIdentifierEquals(c);
+                                    CustomCommand customCommand = byIdentifierEquals.get(0);
+                                    resp = customCommand.getMessage();
+                                }
                             }
-                        }
-                    } else if(content.length()!=0){
-                        Set<String> dbIdentifier = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.toSet());
-                        for (String s : dbIdentifier) {
-                            if (s.contains(content.toLowerCase())) {
-                                List<CustomCommand> byIdentifierEquals = commandRepository.getByIdentifierEquals(content.toLowerCase());
-                                CustomCommand customCommand = byIdentifierEquals.get(0);
-                                resp = customCommand.getMessage();
+                        } else if (content.length() != 0) {
+                            Set<String> dbIdentifier = all.stream().map(CustomCommand::getIdentifier).collect(Collectors.toSet());
+                            for (String s : dbIdentifier) {
+                                if (s.contains(content)) {
+                                    List<CustomCommand> byIdentifierEquals = commandRepository.getByIdentifierEquals(content);
+                                    CustomCommand customCommand = byIdentifierEquals.get(0);
+                                    resp = customCommand.getMessage();
+                                }
                             }
                         }
                     }
-                }
 
-                if (resp.length() != 0) {
-                    String finalResp = resp;
+                    if (resp.length() != 0) {
+                        String finalResp = resp;
 
-                    return Mono.just(eventMessage)
-                            .flatMap(Message::getChannel)
-                            .flatMap(channel -> channel
-                                    .createEmbed(
-                                            spec -> spec
-                                                    .setColor(Color.GREEN)
-                                                    .setAuthor("F.A.Q - BOT", "", "https://www.cookwithmanali.com/wp-content/uploads/2018/04/Vada-Pav.jpg")
-                                                    .setDescription("`" + finalResp + "`")
-                                                    .setTimestamp(Instant.now())
-                                    )
-                            )
-                            .then();
+                        return Mono.just(eventMessage)
+                                .flatMap(Message::getChannel)
+                                .flatMap(channel -> channel
+                                        .createEmbed(
+                                                spec -> spec
+                                                        .setColor(Color.GREEN)
+                                                        .setAuthor("F.A.Q - BOT", "", "https://www.cookwithmanali.com/wp-content/uploads/2018/04/Vada-Pav.jpg")
+                                                        .setDescription("`" + finalResp + "`")
+                                                        .setTimestamp(Instant.now())
+                                        )
+                                )
+                                .then();
+                    }
                 }
             }
-        }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("Exception inside MessageListener : " + e.getMessage());
             e.printStackTrace();
         }
