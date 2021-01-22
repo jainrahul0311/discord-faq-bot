bf7be0920d0cd1d2486fbf8d305c328cf01e4bdd
Rahul Jain <jainrahul0311@gmail.com>
2021-01-22 18:10:41 +0530

    KeepMeAliveUsing KrnoJobs

--- src/main/java/com/discord/faq_bot/KeepMeAliveBean.java
+++ src/main/java/com/discord/faq_bot/KeepMeAliveBean.java
@@ -4,18 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@Component
+@RestController
 public class KeepMeAliveBean {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
-    
-    public KeepMeAliveBean() {
-        logger.info("Inside Keep Alive Bean");
-        while (true){
-            long time = new Date().getTime();
-        }
+    @GetMapping("/")
+    public void keepMeAlive(){
+        logger.info("Keep Me Alive HeartBeat");
     }
 }
